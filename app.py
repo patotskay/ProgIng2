7c4703846e475e24f2a9897370775fd3f6030a3d
termik88 <dirkser@mail.ru>
2023-04-23 16:11:33 +0500

    Файловая структура и работа после ревью

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
 import streamlit as st
 from streamlit.components.v1 import html
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-# модель для анализа злобности
-classifier = pipeline("sentiment-analysis")
+from models.models import get_models
+from utils.language.ru import get_text_content
+from utils.settings import get_settings
 
-# загрузка модели и токенизатора
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+# получим модели
+classifier, tokenizer, model = get_models()
+# получаем текстовый контент
+main_title, sub_title = get_text_content()
+# получаем настроки приложения
+settings = get_settings()
 
-# задаем параметры генерации ответов
-MAX_HISTORY_LEN = 1000
-MAX_ANSWER_LEN = 50
-
-# задаем заголовок страницы и описание приложения
-TITLE = "Чат-бот на основе DialoGPT-medium"
-SUB_TITLE = "Введите свой запрос и получите ответ от чат-бота на основе модели DialoGPT-medium от Microsoft:"
-
-with open("styles.css") as f:
+# собираем страницу CSS из трех частей HTML:
+# 0) добавляем общий файлк с CSS стилями
+with open("static/css/styles.css") as f:
     css = f.read()
 
+# 1) HEADER
 # добавляем HTML-блок general-header
 with open("templates/general/header.html", "r") as f:
     header = f.read()
+html_template = f'<style>{css}</style>\n{header.format(title=main_title, sub_title=sub_title)}'
+html(html_template, width=800, height=200)
 
-html_template = f'<style>{css}</style>\n{header.format(title=TITLE, sub_title=SUB_TITLE)}'
-html(html_template, width=800, height=300)
-
+# 2) MAIN
 # создаем форму для ввода запросов
 user_input = st.text_input("Вы: ")
 
@@ -34,7 +32,7 @@ user_input = st.text_input("Вы: ")
 if user_input:
     # кодируем запрос пользователя и добавляем его в историю чата
     input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
-    chat_history_ids = model.generate(input_ids, max_length=MAX_HISTORY_LEN, pad_token_id=tokenizer.eos_token_id)
+    chat_history_ids = model.generate(input_ids, max_length=settings['MAX_HISTORY_LEN'], pad_token_id=tokenizer.eos_token_id)
 
     # декодируем ответ бота и выводим его на страницу
     bot_response = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
@@ -46,5 +44,6 @@ with open("templates/main/index.html", "r") as f:
 
 label_value = classifier(user_input)[0]['label']
 
+# 3) FOOTER
 html_template = f'<style>{css}</style>\n{index.format(label=label_value)}'
 html(html_template, width=800, height=300)
---
+++ utils/language/ru.py
@@ -0,0 +1,7 @@
+# текстовый контент на русском языке
+MAIN_TITLE = "Чат-бот на основе DialoGPT-medium"
+SUB_TITLE = "Введите свой запрос и получите ответ от чат-бота на основе модели DialoGPT-medium от Microsoft:" 
+
+
+def get_text_content():
+    return MAIN_TITLE, SUB_TITLE
